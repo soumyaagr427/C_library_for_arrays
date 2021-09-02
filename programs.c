1fc2d72f45d49d542d5228041735ff0ace175cbe
Soumya Agrawal <soumya.agr6@gmail.com>
2021-09-02 18:59:39 +0530

    Update programs.c

--- programs.c
+++ programs.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
-
-
+#include"array.h"
+// linked my header file using "" as both are in same directory.
 
 int main()
 {
    int arr[10];
-
+   
+   //performing array functions on the above array
    inputarray(arr,10);
    sort(arr,10);
    search(arr,10,1);
